8b706229a47405230d1ceccefe1c749542f5b12d
fcusumano <francesco.cusumano@tecniplast.it>
2017-05-11 00:47:46 +0200

    Worked on Cartridge

--- src/com/nanni/nes/Cartridge.scala
+++ src/com/nanni/nes/Cartridge.scala
@@ -3,6 +3,12 @@ package com.nanni.nes
 /**
   * Created by Nanni on 08/05/2017.
   */
-class Cartridge {
+class Cartridge(bytes: Array[Byte]) {
+  if(!bytes.take(4).sameElements(Array('N', 'E', 'S', 0x1A))) {
+    throw new Exception("Invalid iNes cartridge!")
+  }
 
+  private val prgSize = bytes(5)
+  private val chrSize = bytes(6)
+  private val mapperNumber = (bytes(6) >> 4) & 0xF
 }
